4fe9c0968cb206dc06189dafc3f13b232480ef7d
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-04-01 19:32:18 +0300

    added spec

--- src/test/java/tests/DemoWebShopTests.java
+++ src/test/java/tests/DemoWebShopTests.java
@@ -3,6 +3,7 @@ package tests;
 import api.Auth;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import spec.Spec;
 
 import java.util.Map;
 
@@ -13,36 +14,29 @@ import static utils.FileUtils.readStringFromFile;
 public class DemoWebShopTests extends TestBase {
     @Test
     void addToWishListTest() {
-        Map<String, String> cookies = new Auth().login();
         String body = readStringFromFile("./src/test/resources/body.txt");
 
-        Response response =
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .cookies(cookies)
-                        .body(body)
-                        .when()
-                        .post("/addproducttocart/details/53/2")
-                        .then()
-                        .log().body()
-                        .statusCode(200)
-                        .body("success", is(true))
-                        .extract().response();
+        Response response = given()
+                .spec(Spec.request(body))
+                .when()
+                .post("/addproducttocart/details/53/2")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("success", is(true))
+                .extract().response();
     }
- @Test
+
+    @Test
     void sendMailToFriendTest() {
-        Map<String, String> cookies = new Auth().login();
         String body = readStringFromFile("./src/test/resources/bodyForMail.txt");
 
-        Response response =
-                given()
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .cookies(cookies)
-                        .body(body)
-                        .when()
-                        .post("/productemailafriend/53")
-                        .then()
-                        .statusCode(200)
-                        .extract().response();
+        Response response = given()
+                .spec(Spec.request(body))
+                .when()
+                .post("/productemailafriend/53")
+                .then()
+                .statusCode(200)
+                .extract().response();
     }
 }
