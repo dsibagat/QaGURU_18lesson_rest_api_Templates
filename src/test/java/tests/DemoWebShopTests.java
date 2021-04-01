a25ecf9071363071319621956578953bb3f09757
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-04-01 19:11:08 +0300

    deleted spec

--- src/test/java/tests/DemoWebShopTests.java
+++ src/test/java/tests/DemoWebShopTests.java
@@ -1,32 +1,48 @@
 package tests;
 
+import api.Auth;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import spec.Spec;
 
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static utils.FileUtils.readStringFromFile;
 
 public class DemoWebShopTests extends TestBase {
     @Test
-    public Response addToWishListTest() {
+    void addToWishListTest() {
+        Map<String, String> cookies = new Auth().login();
         String body = readStringFromFile("./src/test/resources/body.txt");
-        return Spec.request(body).when()
-                .post("/addproducttocart/details/53/2")
-                .then()
-                .log().body()
-                .statusCode(200)
-                .body("success", is(true))
-                .extract().response();
-    }
 
-    @Test
-    public Response sendMailToFriendTest() {
-        String bodyForMail = readStringFromFile("./src/test/resources/bodyForMail.txt");
-        return Spec.request(bodyForMail).when()
-                .post("/productemailafriend/53")
-                .then()
-                .statusCode(200)
-                .extract().response();
+        Response response =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .cookies(cookies)
+                        .body(body)
+                        .when()
+                        .post("/addproducttocart/details/53/2")
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .body("success", is(true))
+                        .extract().response();
+    }
+ @Test
+    void sendMailToFriendTest() {
+        Map<String, String> cookies = new Auth().login();
+        String body = readStringFromFile("./src/test/resources/bodyForMail.txt");
+
+        Response response =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .cookies(cookies)
+                        .body(body)
+                        .when()
+                        .post("/productemailafriend/53")
+                        .then()
+                        .statusCode(200)
+                        .extract().response();
     }
 }
