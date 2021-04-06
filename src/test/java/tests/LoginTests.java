2de24c2bd3714c14a148dfc202bcb4a5ca80879e
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-04-06 23:17:04 +0300

    added tags

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -15,7 +15,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static tameplate.ReportTemplate.filters;
 
-@Tag("test_api")
+@Tag("web")
 public class LoginTests extends TestBaseApi {
 
     @Test
